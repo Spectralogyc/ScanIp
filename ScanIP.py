d22c4dc9a72db0d8b18b82def1b7a5b8786a5a9d
Ygor Costa <ygordamiao3730@gmail.com>
2024-11-15 23:47:50 -0300

    Create ScanIP.py

---
+++ ScanIP.py
@@ -0,0 +1,19 @@
+import socket
+
+def scan_ip(ip_address):
+    try:
+        socket.inet_aton(ip_address)
+        return True
+    except socket.error:
+        return False
+
+def main():
+    start_ip = input("Enter the starting IP address: ")
+    end_ip = input("Enter the ending IP address: ")
+
+    for ip_address in range(int(start_ip), int(end_ip) + 1):
+        if scan_ip(str(ip_address)):
+            print(ip_address)
+
+if __name__ == "__main__":
+    main()
